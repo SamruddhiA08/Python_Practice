4edbdafd8ec3320e0077606e002c59ca5e3d2ef2
Samruddhi Ashtikar <smashtikar0002@gmail.com>
2026-09-05 12:16:20 +0530

    day-1

---
+++ 1Variable.py
@@ -0,0 +1,24 @@
+# Variable = A container for a value (String, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "sam" #variable
+food = "pizza"
+# f"" format string used of print in some format
+#eg-
+print(f"hey {first_name} you like {food}")
+
+#Integers
+age = 21
+print(f"You are {age} years old")
+
+#Float
+
+Price = 10.99
+print(f"the price is ${Price} ")
+
+#Booleans
+
+is_Active = True
+print(f"Are you active? {is_Active}")
+
