66be13101f85b3b439bcafbba524f62ce729c691
Samruddhi Ashtikar <smashtikar0002@gmail.com>
2026-09-18 06:35:23 +0530

    day3

---
+++ 4operators📐.py
@@ -0,0 +1,19 @@
+number = 5
+# arithmatic operators
+# number = number + 1
+# print(f"addition: {number}")
+
+# number = number - 1
+# print(f"substraction: {number}")
+
+ # number = number * 2
+ # print(f"multiply = {number}")
+
+ # number = number/2
+ # print(f"divide = {number}")
+
+ # Augmented -
+ # +=
+ # *=
+ # -=
+ # /= & so on
---
+++ 5mathfunc📐.py
@@ -0,0 +1,14 @@
+# builtin math functions
+
+x = 5.14
+y = 4
+z = 7
+
+# result = round(x)
+# result = abs(y)
+# result = min(x, y, z)
+# result = max(x, y, z)
+# result = sum(x,y,z)
+result = pow(z,2)
+
+print(result)
